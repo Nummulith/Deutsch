ce4561eb07684334b514597d0eb25768f331a6d8
Nummulith <144725587+Nummulith@users.noreply.github.com>
2025-11-12 21:58:10 +0100

    incremental deutsch

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import Qt, QRectF
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../python/'))) # Добавляем в sys.path путь к папке python
-from common import wordsFrom, randomDict
+from common import wordsFrom, randomDict, filesFromFiles, wordsFromFile
 from common_db import set_value, get_value
 
 THEME = "lex"
@@ -18,7 +18,9 @@ FILES = f"./source/{THEME}.files"
 # FILES = f"./course/{THEME}.files"
 DB  = f"./trainer/{THEME}.db"
 
-memoryK = 2.5
+MEMORYUP   = 2.5
+MEMORYDOWN = 3.0
+WINRATIO = 0.5
 
 def ChangeExt(path, new_ext):
     base, _ = os.path.splitext(path)
@@ -49,10 +51,12 @@ class DiagramWidget(QtWidgets.QWidget):
         self.setMinimumHeight(150)
 
     def paintEvent(self, event):
+        wnd = self.window
+
         qp = QtGui.QPainter(self)
         qp.setRenderHint(QtGui.QPainter.Antialiasing)
 
-        words = self.window.index
+        words = wnd.index
         n = len(words)
         if n == 0:
             return
@@ -68,6 +72,8 @@ class DiagramWidget(QtWidgets.QWidget):
 
         now = datetime.now()
 
+        filesExp = 0
+
         # если есть "лишнее" пространство — не центрируем, просто обрезаем
         for i, q in enumerate(words):
             col = i % cols
@@ -76,11 +82,28 @@ class DiagramWidget(QtWidgets.QWidget):
             y = row * size
             if y >= h:  # за границей формы — не рисуем
                 break
-            ratio  = self.window.ratios [q]
-            expire = self.window.expires[q]
+            ratio  = wnd.ratios [q]
+            expire = wnd.expires[q]
             qp.fillRect(QRectF(x, y, size, size), ratio2color(ratio, expire < now))
 
+            if not expire < now:
+                filesExp += 1
+
         qp.end()
+
+        # wnd.label_files.setText(f"{int(wnd.filesCur)} / {int(wnd.filesMax)} | {filesExp} / {int(wnd.ratioMax)} | {wnd.ratioCur / wnd.ratioMax:.5f}")
+
+        # print(f"{wnd.ratioCur:.2f} / {int(wnd.ratioMax)} = {wnd.ratioCur / wnd.ratioMax:.5f}")
+        wnd.label_total.setText(f"remembered {wnd.ratioCur:.2f} / {int(wnd.ratioMax)}")
+        wnd.paintLabel(wnd.label_total, wnd.ratioCur / wnd.ratioMax)
+
+        # print(f"{int(wnd.filesCur)} / {int(wnd.filesMax)} = {wnd.filesCur / wnd.filesMax:.5f}")
+        wnd.label_files.setText(f"files {int(wnd.filesCur)} / {int(wnd.filesMax)}")
+        wnd.paintLabel(wnd.label_files, wnd.filesCur / wnd.filesMax)
+
+        # print(f"{int(filesExp)} / {int(wnd.ratioMax)} = {filesExp / wnd.ratioMax:.5f}")
+        wnd.label_words.setText(f"learned {int(filesExp)} / {int(wnd.ratioMax)}")
+        wnd.paintLabel(wnd.label_words, filesExp / wnd.ratioMax)
 
 def timedelta2ratio(td: timedelta):
     total_seconds = td.total_seconds()
@@ -95,39 +118,76 @@ def timedelta2ratio(td: timedelta):
 def ratio2color(ratio, pale):
     # ratio: 0.0 (начало) → 1.0 (конец)
     # HSV: hue = 0 (красный) → 270 (фиолетовый)
-    hue = int(360 * (ratio if ratio <= 1. else 1.))
+    hue = int(270 * (ratio if ratio <= 1. else 1.))
     saturation = 255 if not pale else 50
     value = 255
 
     return QtGui.QColor.fromHsv(hue, saturation, value)
+
+
 
 class Window(QtWidgets.QMainWindow, uic.loadUiType(ChangeExt(sys.argv[0], ".ui"))[0]):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.words = randomDict(wordsFrom(FILES))
-
-        now = datetime.now()
-
-        self.ratios  = {}
-        self.expires = {}
-        for i, (q, a) in enumerate(self.words.items()):
-            print(".", end="", flush=True)
-            word_data = get_value(DB, q)
-            remembered = word_data.get("remembered") if word_data else timedelta(0)
-            last       = word_data.get("last")       if word_data else now
-            self.expires[q] = last + remembered
-            self.ratios [q] = timedelta2ratio(remembered)
-        print("!")
-        print(len(self.words), "words loaded")
-        self.reindex()
-
         self.diagram = DiagramWidget(self)
-        self.verticalLayout.addWidget(self.diagram)
+        self.lo_diagram.addWidget(self.diagram)
+
+        self.files = filesFromFiles(FILES)
+        self.files = {f: False for f in self.files}
+        self.filesMax = len(self.files)
+        self.words, self.ratios, self.expires = {}, {}, {}
+        self.filesCur, self.ratioMax, self.ratioCur = 0., 0., 0.
+
+        # print("\r\n\r", end="");
+        print(".")
+        self.newFile()
 
         self.phase = "Q"
         self.new_word()
+
+    def newFile(self):
+        if self.filesCur == self.filesMax:
+            return
+
+        now = datetime.now()
+        for filename, _ in self.files.items():
+            if self.files[filename]:
+                continue
+
+            for q, a in wordsFromFile(filename).items():
+                if q in self.words and a != self.words[q]:
+                    print(f"Duplicate word <{q}> in {filename}")
+                    continue
+
+                self.words[q] = a
+
+                word_data = get_value(DB, q)
+                remembered = word_data.get("remembered") if word_data else timedelta(0)
+                last       = word_data.get("last")       if word_data else now
+
+                expire = last + remembered
+                ratio  = timedelta2ratio(remembered)
+
+                self.ratioMax += 1.
+                self.ratioCur += ratio
+
+                self.expires[q] = expire
+                self.ratios [q] = ratio
+
+            self.filesCur += 1.
+            self.files[filename] = True
+
+            # print(self.ratioMax, self.ratioCur, self.ratioCur / self.ratioMax, filename)
+            print(f"{int(self.ratioMax)} questions after loading {filename}")
+
+            if self.ratioCur / self.ratioMax < WINRATIO:
+                break
+
+        self.words = randomDict(self.words)
+
+        self.reindex()
 
     def reindex(self):
         self.index = sorted(self.ratios, key=self.ratios.get, reverse=True)
@@ -158,16 +218,19 @@ class Window(QtWidgets.QMainWindow, uic.loadUiType(ChangeExt(sys.argv[0], ".ui")
         self.result = 1.
         self.update_view()
 
+    def paintLabel(self, label, ratio):
+        palette = label.palette()
+        palette.setColor(label.backgroundRole(), ratio2color(ratio, True))
+        label.setAutoFillBackground(True)
+        label.setPalette(palette)
+
     def update_view(self):
         self.label_question.setText(self.current_question)
+        self.paintLabel(self.label_question, timedelta2ratio(self.remembered))
+
         self.label_answer  .setText(self.current_answer if self.phase == "A" else "")
 
-        palette = self.label_question.palette()
-        palette.setColor(self.label_question.backgroundRole(), ratio2color(timedelta2ratio(self.remembered), True))
-        self.label_question.setAutoFillBackground(True)
-        self.label_question.setPalette(palette)
-
-        self.label_info    .setText(f"{self.last.strftime("%d/%m %H:%M")} | {short_timedelta(self.remembered)}")
+        self.label_word.setText(f"{self.last.strftime("%d/%m %H:%M")} | {short_timedelta(self.remembered)}")
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -185,13 +248,13 @@ class Window(QtWidgets.QMainWindow, uic.loadUiType(ChangeExt(sys.argv[0], ".ui")
             plus  = key == Qt.Key_Plus  or key == Qt.Key_Equal or key == Qt.Key_Up
             minus = key == Qt.Key_Minus or key == Qt.Key_Down
             if ctrl and plus:
-                self.result = memoryK * memoryK * memoryK
+                self.result = MEMORYUP * MEMORYUP * MEMORYUP
             elif ctrl and minus:
-                self.result = 1 / (memoryK * memoryK * memoryK)
+                self.result = 1 / (MEMORYDOWN * MEMORYDOWN * MEMORYDOWN)
             elif plus:
-                self.result = memoryK
+                self.result = MEMORYUP
             elif minus:
-                self.result = 1 / memoryK
+                self.result = 1 / MEMORYDOWN
 
             if self.result != 1.:
 
@@ -215,9 +278,19 @@ class Window(QtWidgets.QMainWindow, uic.loadUiType(ChangeExt(sys.argv[0], ".ui")
         set_value(DB, self.current_question, {"last": now, "remembered": self.remembered})
 
         self.expires[self.current_question] = now + self.remembered
-        self.ratios [self.current_question] = timedelta2ratio(self.remembered)
+
+        ratio = self.ratios [self.current_question]
+        self.ratioCur -= ratio
+
+        ratio = timedelta2ratio(self.remembered)
+        self.ratioCur += ratio
+        self.ratios [self.current_question] = ratio
 
         self.reindex()
+
+        if self.ratioCur / self.ratioMax >= WINRATIO:
+            self.newFile()
+
 
     def draw_diagram(self):
         self.diagram.update()
